481d6bda2a24f5cb7190fbf4d0957a1892186fca
oguzhanascier <oguzhanascier20@gmail.com>
2023-01-02 14:47:50 +0300

    FORMAT

--- src/composables/useLogin.js
+++ src/composables/useLogin.js
@@ -1,17 +1,17 @@
 import { ref } from "vue";
 import { authRef } from "@/firebase/config";
 
-const hata=ref(null) //-burada hataları tutacak
+const hata = ref(null) //-burada hataları tutacak
 
-const login=async(email,parola)=>{ //- email i dışarıdan göndereceğiz 
+const login = async (email, parola) => { //- email i dışarıdan göndereceğiz 
 
-    hata.value=null;
+    hata.value = null;
     try {
-        const res= await authRef.signInWithEmailAndPassword(email,parola) /// authreften sonraki bir method giriş methodumuz tanımlanmış
-        hata.value=null
+        const res = await authRef.signInWithEmailAndPassword(email, parola) /// authreften sonraki bir method giriş methodumuz tanımlanmış
+        hata.value = null
     } catch (error) {
-        
+        hata.value = 'Login başarısız' + ' ' + error.message
     }
- 
+
 
 }
