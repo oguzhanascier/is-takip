8d15325de3535a0a4296062a8d0abeecfc51e841
oguzhanascier <oguzhanascier20@gmail.com>
2023-01-02 14:46:31 +0300

    try catch auth method

--- src/composables/useLogin.js
+++ src/composables/useLogin.js
@@ -2,6 +2,15 @@ import { ref } from "vue";
 import { authRef } from "@/firebase/config";
 
 const hata=ref(null) //-burada hataları tutacak
-const login=async(email,parola)={
-    
+
+const login=async(email,parola)=>{ //- email i dışarıdan göndereceğiz 
+
+    hata.value=null;
+    try {
+        const res= await authRef.signInWithEmailAndPassword(email,parola) /// authreften sonraki bir method giriş methodumuz tanımlanmış
+    } catch (error) {
+        
+    }
+ 
+
 }
