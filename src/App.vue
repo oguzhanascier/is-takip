9421a92648a43796d12e427cfea97eb75af1fac3
oguzhanascier <oguzhanascier20@gmail.com>
2023-01-02 14:13:37 +0300

    content class style

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,13 @@
 <template>
-
-  <router-view/>
+<div class="content">
+  <router-view />
+</div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.content{
+  margin: 0 auto;
+  max-width: 1200px;
+  padding: 0 20px;
 }
 </style>
