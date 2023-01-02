cc3628b79ffe23855993a1c00c2def8eb1bfeb84
oguzhanascier <oguzhanascier20@gmail.com>
2023-01-02 14:57:43 +0300

    submit handle

--- src/views/auth/LoginComp.vue
+++ src/views/auth/LoginComp.vue
@@ -1,20 +1,28 @@
 <template>
-<form action="">
-    <h3>Giriş</h3>
-    <input type="email" placeholder="Email Giriniz" v-model="email">
-    <input type="password" placeholder="Password Giriniz" v-model="parola">
-    <button>Giriş</button>
-    <div v-if="hata" class="error">{{hata}}</div>
-</form>
+    <form  @submit.prevent="handleSubmit">
+        <h3>Giriş</h3>
+        <input type="email" placeholder="Email Giriniz" v-model="email">
+        <input type="password" placeholder="Password Giriniz" v-model="parola">
+        <button>Giriş</button>
+        <div v-if="hata" class="error">{{ hata }}</div>
+    </form>
 </template>
 
 <script>
 import useLogin from '@/composables/useLogin';
+import { ref } from 'vue';
 export default {
-    setup () {
-        
-
-        return {}
+    setup() {
+        let { hata, login } = useLogin()
+        const email = ref('')
+        const parola = ref('')
+        const handleSubmit = async () => {
+            const res = await login(email.value, parola.value)
+            if (!hata.value) {
+                console.log('kullanıcı giriş yaptı');
+            }
+        }
+        return { email, parola, handleSubmit, hata }
     }
 }
 </script>
