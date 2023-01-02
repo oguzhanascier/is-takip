f9e4c74d7d089aa119e3400b09da786c2bc732d4
oguzhanascier <oguzhanascier20@gmail.com>
2023-01-02 14:29:54 +0300

    import firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,26 @@
+import firebase from "firebase/compat/app";
+
+import "firebase/compat/firestore";
+
+import "firebase/compat/auth"; 
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDFVqNoH3zchuEHkAbdqdVpgQLuheBE-CQ",
+
+    authDomain: "blog-app-7cc8a.firebaseapp.com",
+
+    databaseURL: "https://blog-app-7cc8a-default-rtdb.firebaseio.com",
+
+    projectId: "blog-app-7cc8a",
+
+    storageBucket: "blog-app-7cc8a.appspot.com",
+
+    messagingSenderId: "291121184274",
+
+    appId: "1:291121184274:web:f3b236ef31a6a7c5d122cb",
+
+    measurementId: "G-RV1KELVHXM"
+
+  };
+
