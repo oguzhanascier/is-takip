59439e1901b76ea7e8d454f6a9d22a7832a5302d
oguzhanascier <oguzhanascier20@gmail.com>
2023-01-03 14:24:53 +0300

    qweqwe

--- src/composables/getUser.js
+++ src/composables/getUser.js
@@ -1,4 +1,17 @@
 import { ref } from "vue";
 import { authRef } from "@/firebase/config";
 
-const kullanici= ref (authRef.currentUser) ///tanımlanmış fonksiyonla mevcut giriş yapmış kullanıcı bilgisini çektik
+const kullanici = ref(authRef.currentUser) ///tanımlanmış fonksiyonla mevcut giriş yapmış kullanıcı bilgisini çektik
+
+
+authRef.onAuthStateChanged(k => { /// durumun değişip değişmediğini yakalayacağız
+    kullanici.value = k //- çıkış yapmışsa k null olacak
+})
+
+const getUser = () => {
+    return kullanici
+}
+
+
+
+export default getUser
