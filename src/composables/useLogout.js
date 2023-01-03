02647c6c0417e8e124201470d10519021821010e
oguzhanascier <oguzhanascier20@gmail.com>
2023-01-03 14:10:38 +0300

    try catch block

--- src/composables/useLogout.js
+++ src/composables/useLogout.js
@@ -5,5 +5,11 @@ import { authRef } from "@/firebase/config";
 
 const hata = ref(null)
 const logout = async () => {
+    hata.value=null
+    try {
+        
+    } catch (error) {
+        
+    }
 
 }
