6207e4c5ec98072852a8c555e04cfe368b544913
oguzhanascier <oguzhanascier20@gmail.com>
2023-01-03 14:06:14 +0300

    import navbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
-<div class="content">
-  <router-view />
-</div>
+  <div class="content">
+    <router-view />
+  </div>
 </template>
 
+<script>
+import NavbarCompVue from './components/NavbarComp.vue';
+export default {
+  components:{NavbarCompVue},
+  setup() {
+
+  },
+}
+</script>
+
 <style lang="scss">
-.content{
+.content {
   margin: 0 auto;
   max-width: 1200px;
   padding: 0 20px;
