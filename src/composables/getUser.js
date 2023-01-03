abe3498e506e0e2b95a60147fcaaf7f23b98caa2
oguzhanascier <oguzhanascier20@gmail.com>
2023-01-03 14:22:59 +0300

    currentUser method

--- src/composables/getUser.js
+++ src/composables/getUser.js
@@ -1,2 +1,4 @@
 import { ref } from "vue";
 import { authRef } from "@/firebase/config";
+
+const kullanici= ref (authRef.currentUser) ///tanımlanmış fonksiyonla mevcut giriş yapmış kullanıcı bilgisini çektik
