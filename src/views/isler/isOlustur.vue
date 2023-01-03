9097a686028bc5843138eed43f2358c6bac2cc40
oguzhanascier <oguzhanascier20@gmail.com>
2023-01-03 15:24:54 +0300

    created form

--- src/views/isler/isOlustur.vue
+++ src/views/isler/isOlustur.vue
@@ -1,6 +1,13 @@
 <template>
     <div>
-
+        <form @submit.prevent="handleSubmit">
+            <h4>İş Oluştur</h4>
+            <input type="text" placeholder="İşe Başlığı" v-model="baslik">
+            <textarea placeholder="Açıklama Ekle" v-model="aciklama"></textarea>
+            <label> Tanıtıcı Fotoğraf</label>
+            <input type="file">
+            <input type="datetime-local">
+        </form>
     </div>
 </template>
 
