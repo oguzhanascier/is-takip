69900107cfaf92d44fb413f6cead92af3c84dab6
oguzhanascier <oguzhanascier20@gmail.com>
2023-01-03 16:01:56 +0300

    getdolaoad url

--- src/composables/useStorage.js
+++ src/composables/useStorage.js
@@ -2,4 +2,22 @@ import { ref } from "vue";
 import { storageRef } from "@/firebase/config";
 import getUser from './getUser'
 
-const {kullanici}=getUser()
+const { kullanici } = getUser()
+
+const useStorage = () => {
+    const hata = ref(null)
+    const url = ref(null)
+    const fileYol = ref(null)
+
+    const resimYükle = async (file) => { //-resim yüklemek için gerekli fonksiyon
+        fileYol.value=`isler/${kullanici.value.uid}/{file.name}`
+        const storage=storageRef.ref(fileYol.value)
+
+        try {
+            const res= await storage.put(file) ///storage yukarıda oluşturduğumuz sabit
+            url.value= await res.ref.getDownloadURL() ///tanımlanmış fonksiyonla dosya yolu oluşturuldu
+        } catch (error) {
+            
+        }
+    }
+}
