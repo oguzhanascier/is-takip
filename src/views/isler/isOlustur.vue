91df7d38b7b365595fff3ddc2ccd1ba08988a428
oguzhanascier <oguzhanascier20@gmail.com>
2023-01-04 14:02:29 +0300

    qwe

--- src/views/isler/isOlustur.vue
+++ src/views/isler/isOlustur.vue
@@ -34,7 +34,7 @@ export default {
         const handleSubmit = async () => {
             if(file.value){
                 await resimYukle(file.value)
-                console.log(fileYol); //dosya urlesi artık bu yolun içinde
+                console.log(url.value); //dosya urlesi artık bu yolun içinde
                 
             }
         }
