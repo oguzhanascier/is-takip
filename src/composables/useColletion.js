348b33afda69b961e088d1b1b19a2f8964beb4a4
oguzhanascier <oguzhanascier20@gmail.com>
2023-01-04 14:06:06 +0300

    xxxx

--- src/composables/useColletion.js
+++ src/composables/useColletion.js
@@ -1,2 +1,19 @@
 import { ref } from "vue";
 import { firestoreRef } from "@/firebase/config";
+
+const useColletion = (koleksiyon) => {
+    const hataColletion = ref(null)
+    const belgeEkle = async (belge) => {
+        hataColletion.value = null
+        try {
+            await firestoreRef.collection(koleksiyon).add(belge)
+        } catch (error) {
+            hataColletion.value = 'Belge eklerken bir hata oluştu'
+        }
+    }
+
+    return [hataColletion, belgeEkle]
+}
+
+
+export default useColletion
