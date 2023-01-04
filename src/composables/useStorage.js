cd01bc9215eaa877ffaae27703b910bcce4e9804
oguzhanascier <oguzhanascier20@gmail.com>
2023-01-04 13:24:34 +0300

    comment

--- src/composables/useStorage.js
+++ src/composables/useStorage.js
@@ -10,12 +10,12 @@ const useStorage = () => {
     const fileYol = ref(null)
 
     const resimYukle = async (file) => { //-resim yüklemek için gerekli fonksiyon
-        fileYol.value = `isler/${kullanici.value.uid}/${file.name}`
-        const storage = storageRef.ref(fileYol.value)
+        fileYol.value = `isler/${kullanici.value.uid}/${file.name}` /// burada dosya yolu seçildi
+        const storage = storageRef.ref(fileYol.value) ///dosya yolu burada referansa atıldı
 
         try {
             const res = await storage.put(file) ///storage yukarıda oluşturduğumuz sabit
-            url.value = await res.ref.getDownloadURL() ///tanımlanmış fonksiyonla dosya yolu oluşturuldu
+            url.value = await res.ref.getDownloadURL() ///tanımlanmış fonksiyonla burada url oluşturuldu
         } catch (error) {
             hata.value = error;
         }
