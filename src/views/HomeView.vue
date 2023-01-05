fff00b5255a0229efbe6001e27c78731936574f9
oguzhanascier <oguzhanascier20@gmail.com>
2023-01-05 13:37:42 +0300

    import isListesi compoennt

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,16 +4,23 @@
       <div v-if="hataCollection" class="error">
         {{ hataCollection }}
       </div>
+      <div v-if="belgeler">
+        <div v-for="belge in belgeler" :key="belge.id">
+          {{ belge.baslik }}
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import getCollection from '@/composables/getCollection'
+import isListesiVue from '@/components/isListesi.vue'
 export default {
   name: 'HomeView',
+  components: { isListesiVue },
   setup() {
-    const { belgeler, hataCollection } = getCollection()
+    const { belgeler, hataCollection } = getCollection('isler')
 
     return { hataCollection, belgeler }
   }
