2469f67a421e27dc17176a7dcf2c099f30c413ac
oguzhanascier <oguzhanascier20@gmail.com>
2023-01-05 13:37:47 +0300

    sss

--- src/components/isListesi.vue
+++ src/components/isListesi.vue
@@ -1,6 +1,17 @@
 <template>
-    <div>
-
+    <div v-for="is in isler" :key="is.id">
+        <div class="single">
+            <div class="thumbnail">
+                <img :src="is.resimUrl" alt="">
+            </div>
+            <div class="info">
+                <h3>{{ is.baslik }}</h3>
+                <p>Oluşturan : {{ is.kullaniciAd }}</p>
+            </div>
+            <div class="work-number">
+                <p>{{ is.isAdimlar.length }} tane iş adımı bulunuyor.</p>
+            </div>
+        </div>
     </div>
 </template>
 
