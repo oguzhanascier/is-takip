ab172daa7e40222c5bc683eb75a4a0a94df1a9df
oguzhanascier <oguzhanascier20@gmail.com>
2023-01-05 12:32:57 +0300

    access koleksiyonref

--- src/composables/getCollection.js
+++ src/composables/getCollection.js
@@ -4,4 +4,6 @@ import { firestoreRef } from "@/firebase/config";
 const getCollection = (koleksiyon) => {
     const belgeler = ref(null)
     const hataColletion = ref(null)
+
+    let koleksiyonRef = firestoreRef.collection(koleksiyon).orderBy('olusturulmaTarihi', 'desc')
 }
