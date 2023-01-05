9d5dfe386233de7feed2d8450f61df0bea760646
oguzhanascier <oguzhanascier20@gmail.com>
2023-01-05 14:28:16 +0300

    add router link , link to isdetay

--- src/components/isListesi.vue
+++ src/components/isListesi.vue
@@ -1,23 +1,25 @@
 <template>
     <div v-for="is in isler" :key="is.id">
-        <div class="single">
-            <div class="thumbnail">
-                <img :src="is.resimUrl" alt="">
+        <router-link :to="{ name: 'isDetay', params: { id: is.id } }">
+            <div class="single">
+                <div class="thumbnail">
+                    <img :src="is.resimUrl" alt="">
+                </div>
+                <div class="info">
+                    <h3>{{ is.baslik }}</h3>
+                    <p>Oluşturan : {{ is.kullaniciAd }}</p>
+                </div>
+                <div class="work-number">
+                    <p>{{ is.isAdimlar.length }} tane iş adımı bulunuyor.</p>
+                </div>
             </div>
-            <div class="info">
-                <h3>{{ is.baslik }}</h3>
-                <p>Oluşturan : {{ is.kullaniciAd }}</p>
-            </div>
-            <div class="work-number">
-                <p>{{ is.isAdimlar.length }} tane iş adımı bulunuyor.</p>
-            </div>
-        </div>
+        </router-link>
     </div>
 </template>
 
 <script>
 export default {
-    props:['isler'],
+    props: ['isler'],
     setup() {
 
 
