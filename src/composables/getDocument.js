f691587b60b5b3a86048672142775004cbeafea0
oguzhanascier <oguzhanascier20@gmail.com>
2023-01-05 14:51:35 +0300

    xxxx

---
+++ src/composables/getDocument.js
@@ -0,0 +1,25 @@
+import { ref } from "vue";
+import { firestoreRef } from "@/firebase/config";
+
+
+const getDocument = (koleksiyon, id) => {
+    let belge = ref(null)
+    let hataDocument = ref(null)
+
+    let belgeRef = firestoreRef.collection(koleksiyon).doc(id)
+
+    belgeRef.onSnapshot(doc => {
+        if (doc.data) {
+            belge.value = { ...doc.data(), id: doc.id }
+            hataDocument.value = null
+        } else {
+            hataDocument.value = 'Belge Bulunamadı.'
+
+        }
+    }, err => {
+        hataDocument.value = 'Veriye Erişelemedi'
+        console.log(err);
+    })
+    return { hataDocument, belge }
+
+}
