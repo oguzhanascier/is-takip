3ef4a4ee39c26cd34f69de70f80000c27299eeda
oguzhanascier <oguzhanascier20@gmail.com>
2023-01-05 14:22:40 +0300

    solving reflesh problem

--- src/main.js
+++ src/main.js
@@ -3,5 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '../src/assets/main.css'
+import {authRef} from './firebase/config'
 
-createApp(App).use(store).use(router).mount('#app')
+let app;
+
+
+authRef.onAuthStateChanged(()=>{
+    if(!app){
+        app=createApp(App).use(store).use(router).mount('#app')
+    }
+
+})
+
