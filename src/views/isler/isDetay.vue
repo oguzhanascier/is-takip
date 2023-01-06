6d7eab9adaa24a712cdb401e58eed12af464f998
oguzhanascier <oguzhanascier20@gmail.com>
2023-01-06 14:21:36 +0300

    get hataDoc and belge in getDocument compasable

--- src/views/isler/isDetay.vue
+++ src/views/isler/isDetay.vue
@@ -1,14 +1,14 @@
 <template>
     <h3>İş Detay</h3>
-    <p>iş id - {{id}}</p>
+    <p>iş id - {{ id }}</p>
 </template>
 
 <script>
 import getDocument from '@/composables/getDocument';
 export default {
-    props:['id'],
-    setup () {
-        
+    props: ['id'],
+    setup(props) {
+        const { hataDocument, belge: is } = getDocument('isler', props.id)
 
         return {}
     }
