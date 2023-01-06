6b4984d8fccad21a11887f7c3b872549d9f41b31
oguzhanascier <oguzhanascier20@gmail.com>
2023-01-06 16:52:12 +0300

    access isler colletion and comment

--- src/composables/useDocument.js
+++ src/composables/useDocument.js
@@ -4,6 +4,7 @@ import { firestoreRef } from "@/firebase/config";
 
 const useDocument = (koleksiyon, id) => {
     const hata = ref(null)
+    let belgeRef=firestoreRef.collection(koleksiyon).doc(id) /// bununla belgelere eriştik collectionun içine 'işler'i yazsak da olurdu ama parametre olarak göndereceğiz daha güzel hoş ınmmm
 }
 
 
