63d1cb7ef72032f43bce155935963a6de34ea5eb
oguzhanascier <oguzhanascier20@gmail.com>
2023-01-06 16:52:46 +0300

    creadet belgeSil funch

--- src/composables/useDocument.js
+++ src/composables/useDocument.js
@@ -4,7 +4,11 @@ import { firestoreRef } from "@/firebase/config";
 
 const useDocument = (koleksiyon, id) => {
     const hata = ref(null)
-    let belgeRef=firestoreRef.collection(koleksiyon).doc(id) /// bununla belgelere eriştik collectionun içine 'işler'i yazsak da olurdu ama parametre olarak göndereceğiz daha güzel hoş ınmmm
+    let belgeRef = firestoreRef.collection(koleksiyon).doc(id) /// bununla belgelere eriştik collectionun içine 'işler'i yazsak da olurdu ama parametre olarak göndereceğiz daha güzel hoş ınmmm
+
+    const belgeSil = async () => {
+        hata.value = null
+    }
 }
 
 
