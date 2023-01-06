da8f00fec7e121e89a713f5f179f4d9063f377f7
oguzhanascier <oguzhanascier20@gmail.com>
2023-01-06 14:30:08 +0300

    this tag is work details. This tag appeare when 'is' true

--- src/views/isler/isDetay.vue
+++ src/views/isler/isDetay.vue
@@ -3,6 +3,21 @@
     <div v-if="hataDocument" class="error">
         {{ hataDocument }}
     </div>
+    <div v-if="is" class="work-details">
+        <div class="work-info">
+            <div class="image">
+                <img :src="is.resimUrl" alt="">
+            </div>
+            <div>
+                <h2>{{ is.baslik }}</h2>
+            </div>
+            <p class="username">{{ is.kullaniciAd }}</p>
+            <p class="description">{{ is.aciklama }}</p>
+        </div>
+        <div class="work-list">
+            <h2>İş Adımları</h2>
+        </div>
+    </div>
 </template>
 
 <script>
