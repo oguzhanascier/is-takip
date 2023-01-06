7b5eb106167bb43a929dd8ec5cd8ad52e8e7d043
oguzhanascier <oguzhanascier20@gmail.com>
2023-01-06 14:57:24 +0300

    get user in getUser()

--- src/views/isler/isDetay.vue
+++ src/views/isler/isDetay.vue
@@ -23,11 +23,13 @@
 <script>
 import getDocument from '@/composables/getDocument';
 import getUser from '@/composables/getUser';
+import { get } from 'http';
 import { computed } from 'vuex';
 export default {
     props: ['id'],
     setup(props) {
         const { hataDocument, belge: is } = getDocument('isler', props.id)
+        const {kullanici}=getUser()
 
         return { hataDocument, is }
     }
